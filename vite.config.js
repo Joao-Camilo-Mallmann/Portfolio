32a958282c3d20add79ef6a9bd849e0d38cf0527
JC <jcamilomallmann@hotmail.com>
2025-07-16 21:43:35 -0300

    feat: update HTML language attribute and title for portfolio page

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // Nome do repositório para GitHub Pages
   plugins: [
     vue(),
     vueDevTools(),
